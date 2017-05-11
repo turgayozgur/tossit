99e39f177eedebc5d85822d290c37cf8b481c695
turgayozgur <ozgurtrgy@gmail.com>
2017-05-11 23:50:59 +0300

    netstandard16 to netstandard1.6 for builds.

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -e
 for path in src/*/*.csproj;
 do
     dotnet restore $path
-    dotnet build $path -f netstandard16
+    dotnet build $path -f netstandard1.6
 
     # appveyor only:
     [ $APPVEYOR ] \
         && { dotnet build $path -f net451 -c Release; \
-             dotnet build $path -f netstandard16 -c Release; }
+             dotnet build $path -f netstandard1.6 -c Release; }
 done
 
 # test
