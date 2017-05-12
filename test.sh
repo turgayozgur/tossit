816148da402958f326205e24dddbb50b1a5052a5
turgayozgur <ozgurtrgy@gmail.com>
2017-05-12 21:25:17 +0300

    code coverage fix.

--- test.sh
+++ test.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 set -e
 
-# variables
-target=${1:-local}
-
 # run tests on local
-if [ "${target}" == "local" ]; then
+if [ ! $APPVEYOR ]; then
     for path in test/*/*.csproj; do
         dotnet test $path
     done
-fi
-
-# run tests on appveyor
-if [ "${target}" == "appveyor" ]; then
+else
+    # run tests on appveyor
     for path in test/*; do
-        ".\OpenCover\OpenCover.4.6.519\tools\OpenCover.Console.exe" \
+        (cd $path;
+        "..\\..\OpenCover\OpenCover.4.6.519\tools\OpenCover.Console.exe" \
         -oldstyle \
         -target:"C:\Program Files\dotnet\dotnet.exe" \
         -filter:"+[Tossit.*]* -[*.Tests]* -[*]*.Api.*" \
         -targetargs:"test ${path}" \
-        -mergeoutput -output:"../../coverage.xml" -register:user
+        -mergeoutput -output:"..\\..\coverage.xml" -register:user)
     done
 fi
