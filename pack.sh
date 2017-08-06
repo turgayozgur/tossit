477792d5a9a1f6fd1a6edb65148042a5d4d9c207
turgayozgur <ozgurtrgy@gmail.com>
2017-08-06 20:34:11 +0300

    updates to release.

--- pack.sh
+++ pack.sh
@@ -6,5 +6,5 @@ revision=$(printf %04d $APPVEYOR_BUILD_NUMBER)
 
 # pack
 for path in src/*; do
-    dotnet pack ${path} -c Release -o "..\..\artifacts" --version-suffix="beta-${revision}" --no-build
+    dotnet pack ${path} -c Release -o "..\..\artifacts" --no-build
 done
