fe2ee3c58572cab1cb662155dbd359aa6a68490a
turgayozgur <ozgurtrgy@gmail.com>
2018-12-17 00:34:45 +0300

    build config.

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e
 
+configuration=${1-"Debug"}
+
+dotnet restore
+
 # src
 for path in src/*/*.csproj;
 do
     [ $APPVEYOR ] \
-        && { dotnet restore -c Release; \
-             dotnet build $path --no-restore -f net451 -c Release; \
-             dotnet build $path --no-restore -f netstandard2.0 -c Release; } \
-        || { dotnet restore; \
-             dotnet build $path --no-restore -f netstandard2.0 }
+        && { dotnet build $path --no-restore -f net451 -c $configuration; \
+             dotnet build $path --no-restore -f netstandard2.0 -c $configuration; } \
+        || dotnet build $path --no-restore -f netstandard2.0
 done
 
 # test
 for path in test/*/*.csproj;
 do
-    dotnet restore $path
-    dotnet build $path --no-dependencies
+    dotnet build $path -c $configuration --no-dependencies
 done
 
 # samples
 for path in samples/*/*.csproj;
 do
-    dotnet restore $path
-    dotnet build $path
+    dotnet build -c $configuration $path
 done
